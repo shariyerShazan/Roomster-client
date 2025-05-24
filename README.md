d8d9c738f81d804e2299888053677b94db2a1fab
shariyerShazan <shariyershazan1@gmail.com>
2025-05-24 21:54:39 +0600

    last commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+website name : RoomsTer
+
 github : https://github.com/Programming-Hero-Web-Course4/b11a10-client-side-shariyerShazan
+
+liveLink : https://transcendent-rabanadas-8abef7.netlify.app/
+
+
+
+
+## 📁 Technologies Used
+
+- React.js (Frontend)
+- Tailwind CSS (Styling)
+- JSON data (Mock Database)
+- Netlify (Deployment)
+- react-router
+- tostify
+- sweetAlart2
+- axios
+- aos
+- firebase
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -59,7 +59,7 @@ function Navbar() {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow gap-3"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow gap-3 dark:bg-black dark:text-white text-black"
             >
               <li>
                 <NavLink
